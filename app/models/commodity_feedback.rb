dd592d469b1ef4c224694871d1afeb3f554e137e
kuissoft <fivewisdomzero@gmail.com>
2017-04-28 04:27:20 -0400

    Added address resources

--- app/models/commodity_feedback.rb
+++ app/models/commodity_feedback.rb
@@ -1,25 +1,37 @@
 # == Schema Information
 #
-# Table name: commodity_feedbacks
+# Table name: commodities
 #
-#  id           :integer          not null, primary key
-#  description  :string
-#  rate         :integer          not null
-#  user_id      :integer
-#  commodity_id :integer
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id             :integer          not null, primary key
+#  description    :string
+#  picture        :string
+#  dim_w          :decimal(10, 2)
+#  dim_h          :decimal(10, 2)
+#  dim_l          :decimal(10, 2)
+#  distance       :integer          not null
+#  weight         :integer          not null
+#  user_id        :integer
+#  truckload_type :integer
+#  hazard         :boolean          default(FALSE)
+#  price          :decimal(10, 2)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
 #
 # Indexes
 #
-#  index_commodity_feedbacks_on_commodity_id  (commodity_id)
-#  index_commodity_feedbacks_on_user_id       (user_id)
+#  index_commodities_on_truckload_type  (truckload_type)
+#  index_commodities_on_user_id         (user_id)
 #
 
-class CommodityFeedback < ActiveRecord::Base
-  resourcify
+class Commodity < ActiveRecord::Base
   belongs_to :user
-  belongs_to :commodity
+  belongs_to :address_info
+  has_many :commodity_feedbacks, dependent: :destroy
 
-  validates_presence_of :description
+  mount_uploader :picture, CommodityPictureUploader
+  resourcify
+
+  TRUCKLOAD_TYPES = {}
+  validates_presence_of :dim_w, :dim_h, :dim_l, :distance, :description, :price, :pickup_at, :arrive_at
+
 end
