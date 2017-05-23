46e06b754f6fb85b398213b41f47c6d598c5cdb1
kuissoft <fivewisdomzero@gmail.com>
2017-05-23 00:01:12 -0400

    Private bidding hide bidders info

---
+++ app/presenters/api/v1/bid_presenter.rb
@@ -0,0 +1,12 @@
+class Api::V1::BidPresenter < Api::V1::JsonPresenter
+  # http://rubendiazjorge.me/2015/03/23/faster-rails-json-responses-removing-jbuilder-and-view-rendering/
+
+  def self.minimal_hash(bid, current_user)
+    hash = %w(id shipment_id price created_at)
+    node = hash_for(bid, hash)
+    shipment = bid.shipment
+    node[:user] = shipment.private_bidding? ? {id: 0, name: ''} : {id: bid.user_id, name: bid.user.name}
+    node
+  end
+
+end
