290e9b29ebdd01985d64ca01a15fc88d5f58c1af
kuissoft <fivewisdomzero@gmail.com>
2017-07-21 00:14:46 -0400

    manage ship_invitations by shipper

--- app/models/ship_invitation.rb
+++ app/models/ship_invitation.rb
@@ -20,6 +20,8 @@ class ShipInvitation < ActiveRecord::Base
   # Invitee user, may be blank if user not available in our users yet.
   belongs_to :invitee, class_name: 'User', foreign_key: :invitee_id
 
+  scope :for_user, ->(invitee_id) {where(invitee_id: invitee_id)}
+
   validates_format_of :invitee_email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates_presence_of :shipment_id
 
@@ -44,4 +46,9 @@ class ShipInvitation < ActiveRecord::Base
     created
   end
 
+  # IF invitation has been accepted by user registering with this invitation ?
+  # see User#after_create callback
+  def status
+    invitee.present? ? :registered : :pending
+  end
 end
